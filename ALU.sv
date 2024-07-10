e6e9da2e69d9a19ac048c5dd71727a18b1261e53
Shih-Hsuan <113693123+Shih-Hsuan@users.noreply.github.com>
2024-07-11 00:05:40 +0800

    feat: more operation

--- ALU.sv
+++ ALU.sv
@@ -1,29 +1,37 @@
 module ALU
 (	
-	input logic  [3:0]opcode,
+	input logic  [4:0]opcode,
 	input logic  [31:0]op1, op2,
 	input logic  [3:0]flag,
 	output logic [3:0]flag_q,
 	output logic [31:0]result
 );
+	`include "cmds.sv"
+	
 	logic [32:0] res_temp;
 		
 	always_comb
 	begin 
 		unique case(opcode)
-			4'h0: res_temp = op1 + op2;
-			4'h1: res_temp = op1 + op2 + flag[1];
-			4'h2: res_temp = op1 - op2;
-			4'h3: res_temp = op2 - op1;
-			4'h4: res_temp = op1 - op2 - flag[1];
-			4'h5: res_temp = op1 & op2;
-			4'h6: res_temp = op1 | op2;
-			4'h7: res_temp = op1 ^ op2;
-			4'h8: res_temp = op2;
-			4'h9: res_temp = op1 + 1;
-			4'hA: res_temp = op1 - 1;
-			4'hB: res_temp = 0;
-			4'hC: res_temp = ~op1;
+			ADD_OP       : res_temp = op1 + op2;
+			ADD_FLAG_OP  : res_temp = op1 + op2 + flag[1];
+			SUB_OP1      : res_temp = op1 - op2;
+			SUB_OP2      : res_temp = op2 - op1;
+			SUB_FLAG_OP1 : res_temp = op1 - op2 - flag[1];
+			AND_OP       : res_temp = op1 & op2;
+			OR_OP        : res_temp = op1 | op2;
+			XOR_OP       : res_temp = op1 ^ op2;
+			MOV_OP       : res_temp = op2;
+			INC_OP       : res_temp = op1 + 1;
+			DEC_OP       : res_temp = op1 - 1;
+			ZERO_OP      : res_temp = 0;
+			BNOT_OP      : res_temp = ~op1;
+			LSL_OP       : res_temp = op1 << op2;
+			LSR_OP       : res_temp = op1 >> op2;
+			ASR_OP       : res_temp = $signed(op1) >>> op2;
+			ROR_OP       : res_temp = (op1 >> op2) | (op1 << (32 - op2));
+			MUL_OP       : res_temp = op1 * op2;
+			BIC_OP       : res_temp = ~op1 & op2;
 		endcase
 		// flag N,Z,C,V
 		
@@ -31,24 +39,22 @@ module ALU
 			flag_q[2] = res_temp[31:0] == 0;
 		//位移還沒加
 		case(opcode)
-			4'h0: flag_q[1] = res_temp[32];
-			4'h1: flag_q[1] = res_temp[32];
-			4'h2: flag_q[1] = ~res_temp[32];
-			4'h3: flag_q[1] = ~res_temp[32];
-			4'h4: flag_q[1] = ~res_temp[32];
+			ADD_OP       : flag_q[1] = res_temp[32];
+			ADD_FLAG_OP  : flag_q[1] = res_temp[32];
+			SUB_OP1      : flag_q[1] = ~res_temp[32];
+			SUB_OP2      : flag_q[1] = ~res_temp[32];
+			SUB_FLAG_OP1 : flag_q[1] = ~res_temp[32];
 			default: flag_q[1] = flag[1];
 		endcase
 
 		case(opcode)
-			4'h0: flag_q[0] = ((op1[31] == 0 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 1 && op2[31] == 1 && res_temp[31] == 0));
-			4'h1: flag_q[0] = ((op1[31] == 0 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 1 && op2[31] == 1 && res_temp[31] == 0));
-			4'h2: flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 0) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 1));
-			4'h3: flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 0));
-			4'h4: flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 0) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 1));
+			ADD_OP       : flag_q[0] = ((op1[31] == 0 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 1 && op2[31] == 1 && res_temp[31] == 0));
+			ADD_FLAG_OP  : flag_q[0] = ((op1[31] == 0 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 1 && op2[31] == 1 && res_temp[31] == 0));
+			SUB_OP1      : flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 0) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 1));
+			SUB_OP2      : flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 1) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 0));
+			SUB_FLAG_OP1 : flag_q[0] = ((op1[31] == 1 && op2[31] == 0 && res_temp[31] == 0) || (op1[31] == 0 && op2[31] == 1 && res_temp[31] == 1));
 			default: flag_q[0] = flag[0];
 		endcase
-		
-		
 		
 		result = res_temp [31:0];
 	end
