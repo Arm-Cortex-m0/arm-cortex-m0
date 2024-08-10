1b7fdd4024a6af212299c346f360d2d3b7558081
Shih-Hsuan <113693123+Shih-Hsuan@users.noreply.github.com>
2024-08-10 16:07:03 +0800

    feat: Compelte NVCZ flag
    Update ANDS、EORS、LSLS、LSRS、RORS、ORRS、MVNS carry flag

--- ALU.sv
+++ ALU.sv
@@ -17,7 +17,7 @@ module ALU
 			ADD_FLAG_OP  : res_temp = op1 + op2 + flag[1];
 			SUB_OP1      : res_temp = op1 - op2;
 			SUB_OP2      : res_temp = op2 - op1;
-			SUB_FLAG_OP1 : res_temp = op1 - op2 - flag[1];
+			SUB_FLAG_OP1 : res_temp = op2 - op1 - (1 - flag[1]);
 			AND_OP       : res_temp = op1 & op2;
 			OR_OP        : res_temp = op1 | op2;
 			XOR_OP       : res_temp = op1 ^ op2;
@@ -32,9 +32,16 @@ module ALU
 			ROR_OP       : res_temp = (op1 >> op2) | (op1 << (32 - op2));
 			MUL_OP       : res_temp = op1 * op2;
 			BIC_OP       : res_temp = ~op1 & op2;
+			SEH_OP       : res_temp = {{16{op1[15]}}, op1[15:0]}; // 16 bits sign extend
+			SEB_OP       : res_temp = {{24{op1[7]}}, op1[7:0]};   // 8 bits sign extend
+			UEH_OP       : res_temp = {{16'b0}, op1[15:0]}; // 16 bits unsign extend
+			UEB_OP       : res_temp = {{24'b0}, op1[7:0]};   // 8 bits unsign extend
+			REV_OP       : res_temp = {{op1[7:0]}, {op1[15:8]}, {op1[23:16]}, {op1[31:24]}};   
+			REV16_OP     : res_temp = {{op1[23:16]}, {op1[31:24]}, {op1[7:0]}, {op1[15:8]}};   
+			REVSH_OP     : res_temp = {{16{op1[7]}}, {op1[7:0]}, {op1[15:8]}};   
 		endcase
-		// flag N,Z,C,V
 		
+		// flag N,Z,C,V
 			flag_q[3] = res_temp[31];
 			flag_q[2] = res_temp[31:0] == 0;
 		//位移還沒加
@@ -44,7 +51,14 @@ module ALU
 			SUB_OP1      : flag_q[1] = ~res_temp[32];
 			SUB_OP2      : flag_q[1] = ~res_temp[32];
 			SUB_FLAG_OP1 : flag_q[1] = ~res_temp[32];
-			default: flag_q[1] = flag[1];
+            AND_OP       : flag_q[1] = res_temp[32]; // ANDS
+            XOR_OP       : flag_q[1] = res_temp[32]; // EORS
+            LSL_OP       : flag_q[1] = res_temp[32]; // LSLS
+            LSR_OP       : flag_q[1] = res_temp[32]; // LSRS
+            ROR_OP       : flag_q[1] = res_temp[32]; // RORS
+            OR_OP        : flag_q[1] = res_temp[32]; // ORRS
+            BNOT_OP      : flag_q[1] = res_temp[32]; // MVNS
+			default      : flag_q[1] = flag[1];
 		endcase
 
 		case(opcode)
